e0b5291c0b698d88067e67574fcfaf3c853d4a14
Haqiim <hqcoder.jh@gmail.com>
2025-02-17 16:04:42 +0100

    #

--- src/components/httpRequests.jsx
+++ src/components/httpRequests.jsx
@@ -11,47 +11,13 @@ export default function usehttpRequest() {
     //This http request expects a json object that has a property called: message=""
 
 
-    const detectLanguage = async() =>{
+    const detectLanguage = async(query) =>{
         const detector = await window.ai.languageDetector.create();
-        text ="Hola, ¿cómo estás?"
+        text = query
         const results = await detector.detect(text);
-        console.log(results);
+        const language = results[0]
+        console.log(language)
     }
 
-    const getAiReply = async(query,currentTime) =>{
-        function handleError(Er){
-            dispatch(setReplyIsLoading(false))
-            dispatch(setError(true))
-            dispatch(addDialogue({id: dialogue.length + 1, author:'ai', message:Er, time:currentTime }))
-        }
-
-        try {
-            const URL = 'http://localhost:8080/fakeapi' /*input real URL here*/
-
-            const res = await fetch(URL,{
-                method:'POST',
-                body:JSON.stringify({userId:"userId",query:query,time:currentTime}),
-                headers:{
-                    'Content-type': 'application/json'
-                }
-            })
-
-            if(res.status === 200){
-                const response = await res.json()
-                dispatch(setReplyIsLoading(false))
-                dispatch(addDialogue({id: dialogue.length + 1, author:'ai', message:response.data, time:currentTime }))
-            }else{
-                const response = await res.json()
-                handleError(response.error)
-            }
-        } catch (error) {
-            // if(!navigator.onLine){       /*uncomment before deployment*/
-            //     handleError("Check your network connection")
-            //     return
-            // }
-            handleError(error.message)
-        }
-    }
-
-  return{getAiReply, detectLanguage}
+  return{detectLanguage}
 }
--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -20,7 +20,7 @@ export default function Searchbar() {
     const inputRef = useRef(null)
 
     const currentTime = useTimestamp()
-    const {getAiReply, detectLanguage} = usehttpRequest()
+    const {detectLanguage} = usehttpRequest()
 
     const dispatch = useDispatch()
 
@@ -50,8 +50,7 @@ export default function Searchbar() {
         dispatch(setReplyIsLoading(true))
         clearInput()
         setPlaceholder(`ask ${showName}`)
-        detectLanguage()
-        getAiReply(query,currentTime,dialogue)
+        detectLanguage(query)
     }
 
     const sendWithEnter = (e) => {
