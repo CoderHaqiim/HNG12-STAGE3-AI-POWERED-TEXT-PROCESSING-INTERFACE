2c69b6472551526d3aab8d47d8d44493b7a1cb13
Haqiim <hqcoder.jh@gmail.com>
2025-02-20 23:35:45 +0100

    #

--- src/components/reply.jsx
+++ src/components/reply.jsx
@@ -1,5 +1,4 @@
 import useTimestamp from "./usetimestamp"
-import { useState, useEffect } from "react"
 
 export default function Reply({reply, id}) {
   const style={
@@ -13,8 +12,6 @@ export default function Reply({reply, id}) {
     <div className='w-full h-auto flex justify-start gap-[10px]'>
        <div className={`w-[30px] h-[30px] rounded-[50%] flex text-[0.8rem] items-center opacity-[0.6] font-[900] justify-center`} style={style}>AI</div>
       {
-        isLoading? 
-        <LoadingAnimation/>:
           <div className={`py-[10px] text-[1.1rem] px-[10px] min-w-[200px] max-w-[300px] font-[400] rounded-[10px] shadow-shadow1 h-[max-content] leading-[1.2] break-all bg-[#5b046322] lg:max-w-[500px]`}>
             <p>{reply}</p>
           <div className={`mt-[10px] text-[0.6rem] flex items-end justify-end text-[#777777] font-[700]`}>{time}</div>
