1f5a051f306fb5d6cb64c872ec3957bd819a162d
Haqiim <hqcoder.jh@gmail.com>
2025-02-21 06:41:21 +0100

    #

--- src/components/loadingAnimation.jsx
+++ src/components/loadingAnimation.jsx
@@ -10,7 +10,7 @@ export default function LoadingAnimation() {
   }
 
   return (
-      <div className='w-[80px] h-auto justify-between items-center px-[12px] py-[10px] rounded-[20px] flex'>
+      <div className='w-[80px] hidden h-auto justify-between items-center px-[12px] py-[10px] rounded-[20px] lg:flex'>
         <span className='bounce w-[10px] h-[10px] rounded-[50%]' style={style}></span>
         <span className='bounce bounce2 w-[10px] h-[10px] rounded-[50%]' style={style}></span>
         <span className='bounce bounce3 w-[10px] h-[10px] rounded-[50%]' style={style}></span>
