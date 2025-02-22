7348ab7306f40732669f509f4dd33f6bd4150d0e
Haqiim <hqcoder.jh@gmail.com>
2025-02-22 06:08:54 +0100

    #

--- src/components/query.jsx
+++ src/components/query.jsx
@@ -47,7 +47,7 @@ export default function Query({query,id, time}) {
 
   return (
     <div className='w-full h-auto flex justify-end relative query'>
-      <div className={`py-[10px] min-w-[200px] relative text-[1.1rem] px-[10px] query2 max-w-[300px] font-[400] shadow-shadow1 rounded-[10px] break-words leading-[1.2] bg-bluelight lg:max-w-[600px]`}>
+      <div className={`min-w-[200px] relative text-[1.1rem] p-[15px] query2 max-w-[300px] font-[400] shadow-shadow1 rounded-[10px] break-words leading-[1.2] bg-bluelight lg:max-w-[600px]`}>
         <p className="text-[#191964]">{query}</p>
         <div className="w-full relative h-[max-content] mt-[20px] text-[0.6rem] font-bold gap-[10px] flex justify-start">
           <button aria-label="Summarize Input Text" onClick={runSummary} className={`${showSummaryBtn? (detectedLanguage == "en"? "block":"hidden") :"hidden"} w-[max-content] cursor-pointer border-[1px] border-[#191964] px-[8px] rounded-[5px]  bg-[#191964] text-[white] py-[4px] h-[30px]`}>Summarize</button>
--- src/components/reply.jsx
+++ src/components/reply.jsx
@@ -14,7 +14,7 @@ export default function Reply({reply, type, id}) {
     <div className='w-full h-auto flex justify-start gap-[10px]'>
        <div className={`w-[30px] h-[30px] rounded-[50%] flex text-[0.8rem] items-center opacity-[0.6] font-[900] justify-center`} style={style}>AI</div>
       {
-          <div className={`py-[10px] text-[1.1rem] px-[10px] min-w-[200px] max-w-[300px] font-[400] rounded-[10px] shadow-shadow1 h-[max-content] leading-[1.2] break-all bg-[#5b046322] lg:max-w-[500px]`}>
+          <div className={`p-[15px] text-[1.1rem] min-w-[200px] max-w-[300px] font-[400] rounded-[10px] shadow-shadow1 h-[max-content] leading-[1.2] break-all bg-[#5b046322] lg:max-w-[500px]`}>
             <div className="flex justify-between w-full mb-[10px] p-[3px] border-b-[1px] border-[#999999] ">
               <div className="font-bold text-[0.6rem] text-[#333333]" >{type}</div>
               {
